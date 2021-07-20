12f93a90775235dfd44c67c1beefb06b6ae7109c
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2021-07-20 22:29:29 +0100

    Added feed and input component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Feed from "./components/Feed";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
@@ -12,6 +13,7 @@ function App() {
       {/* App body */}
       <div className="app__body">
         <Sidebar />
+        <Feed />
         {/* Feed */}
         {/* Widgets */}
       </div>
